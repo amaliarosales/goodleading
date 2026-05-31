3aaa3847ceb61e6198ac5758fce228c4432d9481
amaliarosales <amalia.r.arenas@gmail.com>
2026-05-31 13:31:05 -0600

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="es">
+      <body className="bg-gray-50">{children}</body>
+    </html>
+  )
+}
